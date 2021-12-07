eff8e3ee8c627ea73904c901bed6df406ccc196f
Simonas Kazlauskas <git@kazlauskas.me>
2021-12-07 14:41:41 +0200

    Add benchmarks for (de-)serialization
    These seem pretty unnecessarily heavy…

--- benches/many_functions.rs
+++ benches/many_functions.rs
@@ -1,34 +1,38 @@
 use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 use wasmer::*;
 
-fn call_many_functions(n: usize) -> String {
+fn call_many_functions(n: usize) -> Vec<u8> {
     let fndefs = (0..n)
         .map(|idx| format!(r#"(func $fn{idx} return)"#, idx = idx))
         .collect::<String>();
     let calls = (0..n)
         .map(|idx| format!("call $fn{idx}\n", idx = idx))
         .collect::<String>();
-    format!(
-        r#"(module {fndefs} (func (export "main") {calls} return) (func (export "single") call $fn0 return))"#,
+    let wat = format!(
+        r#"(module
+            {fndefs}
+            (func (export "main") {calls} return)
+            (func (export "single") call $fn0 return))"#,
         fndefs = fndefs,
         calls = calls
-    )
+    );
+    wat2wasm(wat.as_bytes()).expect("wat must be valid here").to_vec()
 }
 
 fn nops(c: &mut Criterion) {
     for size in [1, 10, 100, 1000, 10000] {
-        let wat = call_many_functions(size);
+        let wasm = call_many_functions(size);
         let store = Store::new(&Universal::new(Singlepass::new()).engine());
         let mut compile = c.benchmark_group("compile");
         compile.bench_with_input(BenchmarkId::from_parameter(size), &size, |b, _| {
             b.iter(|| {
-                let module = Module::new(&store, &wat).unwrap();
+                let module = Module::new(&store, &wasm).unwrap();
                 let imports = imports! {};
                 let _ = Instance::new(&module, &imports).unwrap();
             })
         });
         drop(compile);
-        let module = Module::new(&store, &wat).unwrap();
+        let module = Module::new(&store, &wasm).unwrap();
         let imports = imports! {};
         let instance = Instance::new(&module, &imports).unwrap();
         let mut get_main = c.benchmark_group("get_main");
@@ -57,6 +61,29 @@ fn nops(c: &mut Criterion) {
     }
 }
 
-criterion_group!(benches, nops);
+fn serialization(c: &mut Criterion) {
+    for size in [1, 10, 100, 1000, 10000] {
+        let wasm = call_many_functions(size);
+        let universal = Universal::new(Singlepass::new());
+        let engine = universal.engine();
+        let tunables = BaseTunables::for_target(engine.target());
+        let artifact = engine.compile(&wasm, &tunables).unwrap();
+        let mut serialize = c.benchmark_group("serialize");
+        serialize.bench_with_input(BenchmarkId::from_parameter(size), &size, |b, _| {
+            b.iter(|| artifact.serialize())
+        });
+        let serialized = artifact.serialize().unwrap();
+        drop(serialize);
+
+        let mut deserialize = c.benchmark_group("deserialize");
+        deserialize.bench_with_input(BenchmarkId::from_parameter(size), &size, |b, _| {
+            b.iter(|| unsafe {
+                UniversalArtifact::deserialize(&engine, &serialized).unwrap();
+            })
+        });
+    }
+}
+
+criterion_group!(benches, nops, serialization);
 
 criterion_main!(benches);
